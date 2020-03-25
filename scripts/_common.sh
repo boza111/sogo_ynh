b94521751a22c74fe5453acd951c589f31d25549
Josué Tille <josue@tille.ch>
2020-03-25 23:00:34 +0100

    Add group-permission support and use config helper

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,41 +14,15 @@ install_dependance() {
     ynh_install_app_dependencies sogo stunnel4 memcached
 }
 
-config_sogo() {
-    # Avoid if the directory don't exist
-    mkdir -p /etc/$app
-
-    ynh_backup_if_checksum_is_different --file /etc/$app/sogo.conf
-    cp ../conf/sogo.conf /etc/$app/sogo.conf
-
-    ynh_replace_string --match_string __APP__ --replace_string $app --target_file /etc/$app/sogo.conf
-    ynh_replace_string --match_string __ADMINUSER__ --replace_string $admin --target_file /etc/$app/sogo.conf
-    ynh_replace_string --match_string __DBUSER__ --replace_string $db_user --target_file /etc/$app/sogo.conf
-    ynh_replace_string --match_string __DBPASS__ --replace_string $db_pwd --target_file /etc/$app/sogo.conf
-    ynh_replace_string --match_string __PORT__ --replace_string $port --target_file /etc/$app/sogo.conf
-    ynh_replace_string --match_string __SMTP_PORT__ --replace_string $smtp_port --target_file /etc/$app/sogo.conf
-
-    ynh_store_file_checksum --file /etc/$app/sogo.conf
-}
-
 config_stunnel() {
-    ynh_backup_if_checksum_is_different --file /etc/stunnel/$app.conf
-    cp ../conf/stunnel.conf /etc/stunnel/$app.conf
-
-    ynh_replace_string --match_string __SMTP_PORT__ --replace_string $smtp_port --target_file /etc/stunnel/$app.conf
-
-    ynh_store_file_checksum --file /etc/stunnel/$app.conf
+    ynh_add_config --template="stunnel.conf" --destination="/etc/stunnel/$app.conf"
 
     # Enable stunnel at startup
     ynh_replace_string --match_string "ENABLED=0" --replace_string "ENABLED=1" --target_file /etc/default/stunnel4
 }
 
 config_cron() {
-    ynh_backup_if_checksum_is_different --file /etc/cron.d/$app
-
-    cp ../conf/cron /etc/cron.d/$app
-    ynh_replace_string --match_string __APP__ --replace_string $app --target_file /etc/cron.d/$app
-    ynh_store_file_checksum --file /etc/cron.d/$app
+    ynh_add_config --template="cron" --destination="/etc/cron.d/$app"
     systemctl restart cron
 }
 
